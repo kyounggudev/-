e9e552798d78c21862419c6e3933801558184425
kyounggudev <101247033+kyounggudev@users.noreply.github.com>
2022-03-09 17:17:41 +0900

    Add files via upload

---
+++ project.js
@@ -0,0 +1,12 @@
+function steam(){
+    var ret=confirm("Steam사이트로 이동하시겠습니까?");
+    if(ret==true){
+        location.href="https://store.steampowered.com/";
+    }
+}
+function valve(){
+    var ret=confirm("Valve사이트로 이동하시겠습니까?");
+    if(ret==true){
+        location.href="https://www.valvesoftware.com/en/";
+    }
+}
